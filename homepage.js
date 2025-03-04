b69681a41c8949fa858f44ae04cec597a31bd50f
NiccoloMolinati <niccolo.molinati@gmail.com>
2025-03-04 18:41:19 +0100

    boh

--- homepage.js
+++ homepage.js
@@ -4,18 +4,3 @@ function updateCounter() {
 }
 
 setInterval(updateCounter, 100);
-
-function updateCounter2() {
-    const now = new Date().getTime(); // Ottiene i millisecondi correnti
-    document.getElementById("counter2").textContent = now + 8674562349723; 
-}
-
-setInterval(updateCounter2, 100);
-
-function updateCounter1() {
-    const now = new Date().getTime(); // Ottiene i millisecondi correnti
-    const upd = now + 1234567890123
-    document.getElementById("counter1").textContent = upd; 
-}
-
-setInterval(updateCounter1, 100);

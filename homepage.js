083dcad7fbc3753c7fedf729b92cc749e042a114
NiccoloMolinati <niccolo.molinati@gmail.com>
2025-03-05 16:01:33 +0100

    daje

--- homepage.js
+++
@@ -1,6 +0,0 @@
-function updateCounter() {
-    const now = new Date().getTime(); // Ottiene i millisecondi correnti
-    document.getElementById("counter").textContent = now; 
-}
-
-setInterval(updateCounter, 100);

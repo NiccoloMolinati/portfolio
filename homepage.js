7e3687bb12b10e91b2fed5167b1684e2acb44d3e
NiccoloMolinati <niccolo.molinati@gmail.com>
2025-03-08 15:27:06 +0100

    boh

--- homepage.js
+++ homepage.js
@@ -15,3 +15,35 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+
+document.addEventListener("DOMContentLoaded", function (){
+    document.body.onmousemove = function(e) {
+    document.documentElement.style.setProperty (
+      '--x', (
+        e.clientX+window.scrollX
+      )
+      + 'px'
+    );
+    document.documentElement.style.setProperty (
+      '--y', (
+        e.clientY+window.scrollY
+      ) 
+      + 'px'
+    );
+  }})
+
+
+document.addEventListener("DOMContentLoaded", function (){
+    const cursor = document.querySelector(".invertedcursor");
+    const hoverTargets = document.querySelectorAll(".marquee-container");
+    hoverTargets.forEach(target => {
+    // Quando il mouse entra, ingrandisce il cursore
+    target.addEventListener("mouseenter", () => {
+        cursor.classList.add("cursor-large");
+    });
+
+    // Quando il mouse esce, torna normale
+    target.addEventListener("mouseleave", () => {
+        cursor.classList.remove("cursor-large");
+    });
+});})
